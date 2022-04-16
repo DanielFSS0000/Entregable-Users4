b25542ff406403b6d6103781c7a6488a5ed40396
DanielFSS0000 <daniel97062@gmail.com>
2022-04-15 20:17:19 -0500

    users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import CreateUserButton from './components/CreateUserButton';
+import Modal from './components/Modal';
+import UsersList from './components/UsersList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+	const [openModal, setOpenModal] = useState(false);
+	const [userEdit, setUserEdit] = useState(false);
+
+	const getUsers = () => {
+		axios
+			.get('https://users-crud1.herokuapp.com/users/')
+			.then(res => setUsers(res.data));
+	};
+
+	useEffect(() => {
+		getUsers();
+	}, []);
+
+	const addUser = user => {
+		axios.post('https://users-crud1.herokuapp.com/users/', user).then(() => {
+			getUsers();
+			setOpenModal(false);
+		});
+	};
+
+	const deleteUser = id => {
+		axios
+			.delete(`https://users-crud1.herokuapp.com/users/${id}`)
+			.then(() => getUsers());
+	};
+
+	const selectUser = user => {
+		axios.get(`https://users-crud1.herokuapp.com/users/${user.id}`).then(() => {
+			console.log(user);
+			setUserEdit(user);
+			setOpenModal(true);
+		});
+	};
+
+	const cancelEdit = () => {
+		setUserEdit(false);
+		setOpenModal(false);
+	};
+
+	const updateUser = user => {
+		axios
+			.put(`https://users-crud1.herokuapp.com/users/${user.id}/`, user)
+			.then(() => {
+				setUserEdit(false);
+				setOpenModal(false);
+				getUsers();
+			});
+	};
+
+	return (
+		<div className="App">
+			<h2 className="app-title text-center text-uppercase">Usuarios</h2>
+			<UsersList
+				users={users}
+				deleteUser={deleteUser}
+				selectUser={selectUser}
+				userEdit={userEdit}
+			/>
+			{!!openModal && (
+				<Modal
+					addUser={addUser}
+					userEdit={userEdit}
+					cancelEdit={cancelEdit}
+					updateUser={updateUser}
+				/>
+			)}
+			<CreateUserButton setOpenModal={setOpenModal} />
+		</div>
+	);
 }
 
 export default App;
